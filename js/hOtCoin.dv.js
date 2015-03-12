c0bffa9f8d1e1cd680712253a6de37b8eee61697
brackenavila <luisfernandoavila@outlook.com>
2015-03-12 14:20:52 -0500

    default assets

--- js/hOtCoin.dv.js
+++ js/hOtCoin.dv.js
@@ -1,16 +1,13 @@
-deg=[180,0]
+dg=[180,0]
 F=function(v,f){return Function(v,f)}
 f={
  d:document,
- nw:F('t,a,p,i',
+ nw:F('t,p,i',
   't=f.d.createElement(t);'+
-  'for(i in a)t[i]=a[i];'+
   'return p.appendChild(t)')}
-for(i in deg){
-	d=f.nw('div',0,币)//硬币: Yìng bì (Coin)
- d.style.transform='rotateY('+deg[i]+'deg) translateZ('+i+'px)'
- i=f.nw('img',{src:'img/'+i+'.jpg'},d)
- w=/webkit/i.test(navigator.userAgent)?'-webkit-':''
- s=('#b{background-image:linear-gradient(#000 80%,#800,#000,#800);overflow:hidden_div,img{position:absolute_#区,#币,#币>*,img{height:500px;width:500px_#区{height:200px;left:600px;width:200px_#币{'+w+'animation:watch 10s infinite;transform-style:preserve-3D;transform:rotateY(0deg)_img{border-radius:60%_label{color:#FFF;font-size:3em_@'+w+'keyframes watch{to{transform:rotateY(1440deg)}').split('_')
- x=f.d.styleSheets[0]
- for(i in s)x.insertRule(s[i],x.cssRules.length)}
+for(i in dg)d=f.nw('div',币)//硬币: Yìng bì (Coin)
+w=/webkit/i.test(navigator.userAgent)?'-webkit-':''
+s=('#b{background-image:linear-gradient(#000 80%,#800,#000,#800);overflow:hidden_label{color:#FFF;font-size:3em_div{position:absolute_#区{height:200px;left:600px;width:200px_#币,#币 *{height:500px;width:500px_#币{'+w+'animation:watch 10s infinite;transform-style:preserve-3D_#币 *{border-radius:60%_#币>:first-child{background:url(img/0.jpg)0/contain;transform:translateZ(1px)_#币>:last-child{background:url(img/1.jpg)0/contain;transform:rotateY(180deg)_@'+w+'keyframes watch{to{transform:rotateY(1440deg)}').split('_')
+x=f.d.styleSheets[0]
+for(i in s)x.insertRule(s[i]+'!Important',x.cssRules.length)
+setTimeout(F('','console.clear(),f.d.scripts[0].remove()'),1e3)
